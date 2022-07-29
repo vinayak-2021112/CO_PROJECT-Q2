1bae3a4bd82338d30a5d9be9cf4513c8f271ce7f
VinayakG311 <96966973+VinayakG311@users.noreply.github.com>
2022-07-29 13:27:27 +0530

    Update Assembler_with_Q3.py

--- Assembler_with_Q3.py
+++ Assembler_with_Q3.py
@@ -1,19 +1,19 @@
 try:
-    # instruction = {"add": "10000", "sub": "10001", "movi": "10010", "movr": "10011", "ld": "10100", "st": "10101",
-    #                 "mul": "10110", "div": "10111", "rs": "11000", "ls": "11001", "xor": "11010", "or": "11011",
-    #                 "and": "11100", "not": "11101", "cmp": "11110", "jmp": "11111", "jlt": "01100", "jgt": "01101",
-    #                 "je": "01111", "hlt": "01010", "var": "00000"}
-    
-    instruction = {"add": "00000", "sub": "00001", "ld": "00100", "st": "00101", "mul": "00110",
-                   "div": "00111", "rs": "01000", "ls": "01001", "xor": "01010", "or": "01011", "and": "01100",
-                   "not": "01101",
-                   "cmp": "01110", "jmp": "01111", "jlt": "10000", "jgt": "10001", "je": "10010", "hlt": "10011",
-                   "movi": "00010", "movr": "00011"
-                   }
+    instruction = {"add": "10000", "sub": "10001", "movi": "10010", "movr": "10011", "ld": "10100", "st": "10101",
+                    "mul": "10110", "div": "10111", "rs": "11000", "ls": "11001", "xor": "11010", "or": "11011",
+                    "and": "11100", "not": "11101", "cmp": "11110", "jmp": "11111", "jlt": "01100", "jgt": "01101",
+                    "je": "01111", "hlt": "01010", "var": "00000"}
+
+    # instruction = {"add": "00000", "sub": "00001", "ld": "00100", "st": "00101", "mul": "00110",
+    #                "div": "00111", "rs": "01000", "ls": "01001", "xor": "01010", "or": "01011", "and": "01100",
+    #                "not": "01101",
+    #                "cmp": "01110", "jmp": "01111", "jlt": "10000", "jgt": "10001", "je": "10010", "hlt": "10011",
+    #                "movi": "00010", "movr": "00011"
+    #                }
     register = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100", "R5": "101", "R6": "110",
                 "FLAGS": "111"}
     reg = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100", "R5": "101", "R6": "110"}
-    
+
     flag = "0000000000000000"
     dictreg = {"R0": 0, "R1": 0, "R2": 0, "R3": 0, "R4": 0, "R5": 0, "R6": 0}
     ErrorArray = []
@@ -34,50 +34,50 @@ try:
                 continue
             s = " ".join(s.split())
             f.write(s + "\n")
-    
+
         except EOFError:
             break
     f.close()
     f = open("Myfile.txt", "r")
-    
+
     # extracting individual lines from file
     s = f.read().split("\n")
     s = s[:len(s) - 1]
-    
+
     pcNo = len(s)
     leng_instruction = len(s)
     leng_instruction_og = len(s)
     ErrorFlag = 0
     addtoLabel = True
-    
-    
+
+
     def to_binary(num):
         s = str(bin(num))
         binary_num = s[2:]
         if (len(binary_num) < 8):
             binary_num = (3 - len(binary_num)) * '0' + binary_num
         return binary_num
-    
-    
+
+
     wxx = ''
     pqr = 0
-    
-    
+
+
     def rec(x):
         global wxx, pqr
         s = x / (10 ** len(str(x)))
         s1 = str(s).split(".")
         pqr += 1
-    
+
         if (s1[1]) == '0' or pqr > 5:
             return
         s2 = str(((int(s1[1]) * 2) / 10 ** len(str(s1[1])))).split(".")
-    
+
         wxx += s2[0]
-    
+
         rec(int(s2[1]))
-    
-    
+
+
     def funcfordecimal(x):
         s = str(x).split(".")
         l = int(s[0])
@@ -86,19 +86,19 @@ try:
         rec(r)
         s1 = str(lb)
         s2 = str('0'*(5-len(wxx))+wxx)
-    
+
         return (s1 + s2)
-    
-    
+
+
     def flag_str_to_int(s):
         u = 0
         for l in range(len(s)):
-    
+
             u += int(s[l])#(2 * (len(s) - l - 1))
-    
+
         return u
-    
-    
+
+
     def binary(n):
         s = ''
         b = 0
@@ -106,57 +106,58 @@ try:
             s += str(n % 2)
             n = n // 2
         return "0" * (8 - len(s)) + s[::-1]
-    
-    
-    
-    
+
+
+
+
     count = 0
-    
-    
+
+
     def apply(i):
         global pcNo, ErrorFlag, flag, addtoLabel, leng_instruction, x, halt_count
         k = i.split(" ")
         string = ""
         if list(k[0]) == []:
             k = k[1:]
-    
-    
+
+
         if k[0] == "var":
             try:
                 assert len(k) == 2
                 variables[k[1]] = leng_instruction - x
                 leng_instruction += 1
+
             except AssertionError:
                 ErrorArray.append(f"ERROR: Incorrect variable syntax at line {line_counter + 1}")
                 ErrorFlag += 1
                 return
         # seperate conditions for miv immediate and mov register
-    
+
         # flag error handling
         if len(k) > 1:
             if "FLAGS" in k:
                 try:
                     # assert k[0] == "mov" and k[2] in reg
                     # dictreg[k[2]] = flag_str_to_int(flag)
-                    assert k[0] == "mov" and k[1] in reg
-                    dictreg[k[1]] = flag_str_to_int(flag)
+                    assert k[0] == "mov" and k[2] in reg
+                    dictreg[k[2]] = flag_str_to_int(flag)
                 except AssertionError:
                     ErrorArray.append(f"ERROR: Illegal use of flag register at line {line_counter + 1}")
                     ErrorFlag += 1
                     return
-    
-        if k[0] in ["mov","movf"] and k[2] not in register.keys():
+
+        if k[0] == "mov" and k[2] not in register.keys():
             try:
                 assert k[2][0] == "$"
             except AssertionError:
                 ErrorArray.append(f"Error: Immediate value has incorrect syntax at line {line_counter + 1}")
                 ErrorFlag += 1
                 return
-    
+
             try:
                 if ("." in k[2][1:]):
                     string += "00010" + register[k[1]] + funcfordecimal((k[2][1:]))
-    
+
                 else:
                     assert int(k[2][1:]) <= 255
                     assert int(k[2][1:]) >= 0
@@ -165,11 +166,11 @@ try:
                 ErrorArray.append(f"ERROR: Illegal immediate value at line {line_counter + 1}")
                 ErrorFlag += 1
                 return
-    
+
         if k[0] == "mov" and k[2] in register.keys():
             string += instruction["movr"] + "00000" + register[k[1]] + register[k[2]]
-    
-        if k[0] in ["add", "sub", "mul", "xor", "or", "and","addf","subf"]:
+
+        if k[0] in ["add", "sub", "mul", "xor", "or", "and"]:
             try:
                 assert len(k) >= 4
                 if k[1] not in register.keys() or k[2] not in register.keys() or k[3] not in register.keys():
@@ -177,13 +178,13 @@ try:
                     addtoLabel = False
                     return
                 if ("." in register[k[2]] or "." in register[k[3]]):
-                    if (k[0] == "addf"):
+                    if (k[0] == "add"):
                         string += "00000" + "00" + register[k[1]] + register[k[2]] + register[k[3]]
                     else:
-                        string += "000001" + "00" + register[k[1]] + register[k[2]] + register[k[3]]
+                        string += "0000" + "00" + register[k[1]] + register[k[2]] + register[k[3]]
                 else:
                     string += instruction[k[0]] + "00" + register[k[1]] + register[k[2]] + register[k[3]]
-    
+
                 if k[0] in ["add", "sub", "mul"]:
                     if k[0] == ("add" or "sub" or "mul") and dictreg[k[1]] == -1:
                         flag[-3] = 1
@@ -192,7 +193,7 @@ try:
                 ErrorFlag += 1
                 addtoLabel = False
                 return
-    
+
         if k[0] in ["ls", "rs"]:
             if k[1] not in reg.keys():
                 ErrorArray.append(f"ERROR:Invalid instruction syntax at line {line_counter + 1}")
@@ -207,7 +208,7 @@ try:
                 return
         if k[0] in ["div", "not", "cmp"]:
             string += instruction[k[0]] + "00000" + register[k[1]] + register[k[2]]
-    
+
         if k[0] in ["ld", "st"]:
             # dictreg stuff is left
             try:
@@ -224,7 +225,7 @@ try:
                 ErrorFlag += 1
         if k[0] in ["jmp", "jlt", "jgt", "je"] and k[1] in label.keys():
             string += instruction[k[0]] + "000" + label[k[1]][0]
-    
+
         if k[0] in ["jmp", "jlt", "jgt", "je"] and k[1] not in label.keys():
             ErrorArray.append(f"ERROR: Invalid memory address at line {line_counter + 1}")
             ErrorFlag += 1
@@ -234,25 +235,25 @@ try:
             try:
                 assert halt_count == 1
                 string += instruction[k[0]] + "00000000000"
-    
+
             except AssertionError:
                 ErrorArray.append(f"ERROR: Multiple halt instructions used")
                 ErrorFlag += 1
         arr.append(string)
-    
-    
+
+
     def check_initial_label(i):
         global line_counter, addtoLabel, x, arr
         j = 0
         while (i[j] == " "):
             j += 1
         i = i[j:]
-    
+
         k = i.split(" ")
         if k[0] == "mov" or k[0] == "var":
             return
         elif k[0] not in instruction.keys():
-    
+
             if k[0][-1] == ":":
                 if len(k) == 1:
                     return
@@ -262,17 +263,17 @@ try:
                     j = j + 1
                 # if (i[lb_len] == " ") and (i[lb_len + 1] != " "):
                 if k[j] in instruction.keys() or k[j] == "mov":
-    
+
                     d = []
-    
+
                     d.append(binary(line_counter - x))
-    
+
                     label[i[0:lb_len - 1]] = d
                     apply(str(i)[lb_len + j:])
-    
+
                     if addtoLabel:
                         d.append(arr[-1])
-    
+
                         label[i[0:lb_len - 1]] = d
                         return
                 else:
@@ -281,21 +282,21 @@ try:
                 return 0
         else:
             return 0
-    
-    
+
+
     x = 0
-    
+
     for i in s:
         k = i.split(" ")
         if k[0] == "var":
             x = x + 1
-    
+
     for i in s:
-    
+
         k = i.split(" ")
         if k[0] == "var":
             variables[k[1]] = 0
-    
+
         if (ErrorFlag == 0):
             check_initial_label(i)
             line_counter += 1
@@ -304,10 +305,10 @@ try:
     halt_count = 0
     pcNo -= x
     line_counter = 0
-    
-    
+
+
     # checking if the immediate value is > 8bit
-    
+
     def function(s):
         global line_counter, ErrorFlag
         for i in s:
@@ -321,7 +322,7 @@ try:
                 j += 1
             i = i[j:]
             k = i.split(" ")
-    
+
             try:
                 assert i.split(" ") != ['']
                 j = 0
@@ -335,27 +336,27 @@ try:
                 elif lab_len == 0:
                     apply(str(i)[lab_len:])
                     line_counter += 1
-    
+
                 else:
                     # ERROR HANDLING
                     line_counter += 1
                     continue
-    
-    
+
+
             except AssertionError:
                 ErrorArray.append(f"ERROR: Blank Line at {line_counter + 1}")
                 ErrorFlag += 1
-    
-    
+
+
     def check_labels(i):
         global count, ErrorFlag
         j = 0
         while (i[j] == " "):
             j += 1
         i = i[j:]
-    
+
         k = i.split(" ")
-    
+
         if k[0] == "mov" or k[0] == "var":
             return 0
         elif k[0] not in instruction.keys():
@@ -364,20 +365,20 @@ try:
                     ErrorFlag += 1
                     ErrorArray.append(f"ERROR: No instruction after label at line {line_counter + 1}")
                     return -1
-    
+
                 label_len = len(k[0])
                 j = 1
                 while (k[j] == ''):
                     j = j + 1
                 # if (i[lb_len] == " ") and (i[lb_len + 1] != " "):
-    
+
                 if k[j] in instruction.keys() or k[j] == "mov":
                     return label_len + j
                 else:
                     ErrorFlag += 1
                     ErrorArray.append(f"ERROR: Invalid label syntax at line {line_counter + 1}")
                     return -1
-    
+
             elif len(k) != 1 and k[1] == ":":
                 if k[1] == ":":
                     ErrorFlag += 1
@@ -394,15 +395,15 @@ try:
                     ErrorFlag += 1
                     ErrorArray.append(f"ERROR: Invalid instruction syntax at line {line_counter + 1}")
                     return -1
-    
+
             else:
                 ErrorFlag += 1
                 ErrorArray.append(f"ERROR: Invalid instruction syntax at line {line_counter + 1}")
                 return -1
         else:
             return 0
-    
-    
+
+
     arr = []
     variables = {}
     leng_instruction = leng_instruction_og
@@ -412,10 +413,10 @@ try:
         ErrorArray = []
         ErrorFlag = 0
         function(s)
-    
+
     hltFlag = 0
     # print(x,len(s),s)
-    
+
     for i in range(x, len(s)):
         #  print(i)
         try:
@@ -425,11 +426,11 @@ try:
                 j += 1
             s[i] = s[i][j:]
             k = s[i].split(" ")
-    
+
             if k[0] == "var":
                 ErrorFlag += 1
                 ErrorArray.append(f"ERROR: Var declared at incorrect position at line {i + 1}")
-    
+
             if i == len(s) - 1:
                 if k[0] != "hlt":
                     hltFlag += 1
@@ -440,14 +441,14 @@ try:
         except AssertionError:
             ErrorArray.append(f"ERROR,Blank Line at {i + 1}")
             ErrorFlag += 1
-    
+
     if (len(arr) > 0 and ErrorFlag == 0):
-        if arr[-1] == "1001100000000000":
+        if arr[-1] == "0101000000000000":
             pass
         else:
             ErrorFlag += 1
             ErrorArray.append("ERROR: hlt not in given file")
-    
+
     if ErrorFlag == 0 and len(ErrorArray) == 0:
         for i in range(x, len(arr)):
             print(arr[i])
